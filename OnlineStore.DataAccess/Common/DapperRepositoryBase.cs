5f2e61d86a42e943f46abe45fd0f7832ada66e9a
Dmitry <diman_chelyad@mail.ru>
2024-09-08 19:11:12 +0300

    Начал реализовывать подключение к Postgry через Dapper

---
+++ OnlineStore.DataAccess/Common/DapperRepositoryBase.cs
@@ -0,0 +1,27 @@
+﻿using OnlineStore.AppServices.Common;
+using System.Data;
+using Npgsql;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+
+namespace OnlineStore.DataAccess.Common
+{
+    public abstract class DapperRepositoryBase<T> : IRepository<T> where T : class
+    {
+        private readonly IDbConnection _connection;
+
+        protected DapperRepositoryBase(IConfiguration configuration)
+        {
+            _connection = new NpgsqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(int id)
+        {
+            return _connection.QuerySingleOrDefaultAsync<T>($"SELECT * FROM {typeof(T).Name}s where id = {id}");
+        }
+    }
+}
---
+++ OnlineStore.Domain/Entities/Attribute.cs
@@ -0,0 +1,18 @@
+﻿namespace OnlineStore.Domain.Entities
+{
+    /// <summary>
+    /// Атрибут товара
+    /// </summary>
+    public sealed class Attribute
+    {
+        /// <summary>
+        /// Идентифекатор
+        /// </summary>
+        int Id { get; set; }
+
+        /// <summary>
+        /// Наименование
+        /// </summary>
+        public string Name { get; set; }
+    }
+}

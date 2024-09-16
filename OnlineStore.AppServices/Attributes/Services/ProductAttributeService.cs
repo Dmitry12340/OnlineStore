aeba86a31071cfb498e2c65ea5660d64068c077a
Dmitry <diman_chelyad@mail.ru>
2024-09-16 22:20:46 +0300

    Подключил Redis

---
+++ OnlineStore.AppServices/Attributes/Services/ProductAttributeService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using OnlineStore.AppServices.Attributes.Repositories;
+using OnlineStore.AppServices.Common.Redis;
+using OnlineStore.Contracts;
+
+namespace OnlineStore.AppServices.Attributes.Services
+{
+    /// <summary>
+    /// Сервис по работе с атрибутами продукта.
+    /// </summary>
+    public class ProductAttributeService : IProductAttributeService
+    {
+        private readonly IAttributesRepository _attributesRepository;
+        private readonly IMapper _mapper;
+        private readonly IRedisCache _redisCache;
+
+        /// <inheritdoc/>
+        public ProductAttributeService(
+            IAttributesRepository attributesRepository,
+            IMapper mapper,
+            IRedisCache redisCache)
+        {
+            _attributesRepository = attributesRepository;
+            _mapper = mapper;
+            _redisCache = redisCache;
+        }
+
+        /// <inheritdoc/>
+        public async Task<ProductAttributeDto> GetAsync(int id)
+        {
+            var cacheValue = await _redisCache.GetAsync($"ProductAttributes_{id}");
+
+            if ( cacheValue != null)
+            {
+                return new ProductAttributeDto
+                {
+                    Id = id,
+                    FullAttributeName = cacheValue
+                };
+            }
+
+            var attribute = await _attributesRepository.GetAsync(id)
+                ?? throw new Exception($"Не найден атрибус с id = {id}");
+
+            var dto = _mapper.Map<ProductAttributeDto>(attribute);
+
+            await _redisCache.SetStringAsync($"ProductAttributes_{dto.Id}", $"{dto.FullAttributeName}");
+
+            return dto;
+        }
+    }
+}
---
+++ OnlineStore.Domain/Entities/ProductAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Domain.Entities
+{
+    /// <summary>
+    /// Аттрибут товара.
+    /// </summary>
+    public class ProductAttribute
+    {
+        public object name;
+
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Наименование.
+        /// </summary>
+        public string Name { get; set; } = default!;
+    }
+}

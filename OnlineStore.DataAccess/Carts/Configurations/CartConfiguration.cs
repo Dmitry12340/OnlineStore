85d0af48112102eb4dad12a4c9ff862934ea7625
Dmitry <diman_chelyad@mail.ru>
2024-10-27 23:36:48 +0300

    Добавил сущности корзины, добавил конфигурацию сущностей корзины

---
+++ OnlineStore.DataAccess/Carts/Configurations/CartConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.DataAccess.Carts.Configurations
+{
+    public sealed class CartConfiguration : IEntityTypeConfiguration<Cart>
+    {
+        public void Configure(EntityTypeBuilder<Cart> builder)
+        {
+            builder.ToTable("cart");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(t => t.Created)
+                .IsRequired(true);
+
+            builder.Property(t => t.Updated)
+                .IsRequired(false);
+
+            builder.Property(t => t.Closed)
+                .IsRequired(false);
+        }
+    }
+}
---
+++ OnlineStore.DataAccess/Carts/Configurations/CartProductConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.DataAccess.Carts.Configurations
+{
+    public sealed class CartProductConfiguration : IEntityTypeConfiguration<CartProduct>
+    {
+        public void Configure(EntityTypeBuilder<CartProduct> builder)
+        {
+            builder.ToTable("cartproduct");
+
+            builder.HasKey(x => x.Id);
+
+            builder.HasOne(t => t.Cart)
+                .WithMany()
+                .HasForeignKey(t => t.CartId)
+                .IsRequired(true);
+
+            builder.HasOne(t => t.Products)
+                .WithMany()
+                .HasForeignKey(t => t.ProductId)
+                .IsRequired(true);
+        }
+    }
+}

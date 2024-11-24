bf5bd4292528c219fe827f99120cb903f533a129
diman <diman@DMITRY>
2024-11-24 17:37:21 +0300

    Реализовал паттерн Mediator, реализовал пока только для ProductController метод Add

---
+++ OnlineStore.AppServices/Mediator/Product/Commands/AddProductCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using OnlineStore.Contracts.ProductsDto;
+
+namespace OnlineStore.AppServices.Mediator.Product.Commands
+{
+    public class AddProductCommand : IRequest
+    {
+        public ProductsDto _productDto { get; }
+
+        public AddProductCommand(ProductsDto productDto)
+        {
+            _productDto = productDto;
+        }
+    }
+}
---
+++ OnlineStore.AppServices/Mediator/Product/Handlers/AddProductCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using OnlineStore.AppServices.Mediator.Product.Commands;
+using OnlineStore.AppServices.Product.Repositories;
+using OnlineStore.AppServices.Product.Services;
+
+namespace OnlineStore.AppServices.Mediator.Product.Handlers
+{
+    public class AddProductCommandHandler : IRequestHandler<AddProductCommand>
+    {
+        private readonly IProductService _productService;
+        public AddProductCommandHandler(IProductRepository repository, IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task Handle(AddProductCommand request, CancellationToken cancellationToken)
+        {
+            await _productService.AddAsync(request._productDto);
+        }
+    }
+}

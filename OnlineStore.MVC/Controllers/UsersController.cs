215f672a3c9db7c320e4767b33ad4a94c0cf4d88
diman <diman@DMITRY>
2024-12-03 23:30:06 +0300

    Добавил вьюшку для отображения списка пользователей

--- OnlineStore.MVC/Controllers/UsersController.cs
+++ OnlineStore.MVC/Controllers/UsersController.cs
@@ -1,18 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OnlineStore.AppServices.UserRoles.Services;
+using OnlineStore.AppServices.Authentication.Services;
+using OnlineStore.Contracts.Users;
 
 namespace OnlineStore.MVC.Controllers
 {
     public class UsersController : Controller
     {
         private readonly IUserRolesService _userRolesService;
-        public UsersController(IUserRolesService userRolesService)
+        private readonly IAuthenticationService _authenticationService;
+        public UsersController(IUserRolesService userRolesService,
+            IAuthenticationService authenticationService)
         {
             _userRolesService = userRolesService;
+            _authenticationService = authenticationService;
         }
-        public IActionResult Index()
+        public IActionResult Index(CancellationToken cancellation)
         {
-            return View();
+            var listUsers = _authenticationService.GetAll(cancellation);
+            var listUserDto = new List<UserDto>();
+            foreach (var user in listUsers)
+            {
+                listUserDto.Add(new UserDto { UserName = user.UserName, Id = user.Id });
+            }
+            return View(listUserDto);
         }
     }
 }
